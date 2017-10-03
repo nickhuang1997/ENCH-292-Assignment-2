185d285ec71d454671b3ae2ada1cfae3ee3f393d
NickHuangLaptop <njgh002@bucknell.edu>
2017-10-03 15:19:55 +1300

    setting parameters

---
+++ PH_NH_Assgn2_File.m
@@ -0,0 +1,22 @@
+%   |\\\\\\\\\\\\\\\\\\\\\\\\\\\|  
+%   |___________________________|   _
+%   |                           |   |
+%   |                           |   |      
+%   |                           |   |      
+%   |_____                      |   |  H
+%   |     |                     |   |
+%   |_____|                     |   |
+%   |                           |   |
+%   |___________________________|   _
+%               L
+
+%%
+%setting parameters of materials
+w = .03 %m
+h = .04 %m
+
+Dx = 0.0005 %.5 mm cell width and height
+k_copper  =        %thermal conductivity of copper wire
+k_insul   =        %thermal conductivity of insulation
+k_tile    =        %thermal conductivity of tile
+
