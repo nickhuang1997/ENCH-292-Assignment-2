839bcf9e334956c288b6ee4da21dc2b4a0624b2f
nickhuang1997 <njgh002@bucknell.edu>
2017-10-12 19:00:57 +1300

    before biot

--- PH_NH_Assgn2_File.m
+++ PH_NH_Assgn2_File.m
@@ -19,7 +19,10 @@ Dx = 0.0005;       %.5 mm cell width and height
 k_c       = 385;     %thermal conductivity of copper wire   %%%%% engr toolbox
 k_i       = 2;     %thermal conductivity of insulation
 k_t       = 4;     %thermal conductivity of tile
-k_air     = 0.024;
+k_air     = 0.0257; %from engineering toolbox
+hc = 19.91          %k*Nu*z (average HTC for forced convection of model)
+
+Bi = hc*Dx/k_air
 
 %400/.0002;
 q_wire    = 400/0.000025 
@@ -246,33 +249,33 @@ for iN = 1:length(idx)
             
         elseif iR > 10 && iR < 20 && iC < 2%do range for all left walls of copper
             cas = 20;
-            Adum(iR,iC:iC+1) = [-3 k_c];
-            Adum(iR+1,iC)    = k_c;
-            Adum(iR-1,iC)    = k_c;
-            b(iN) = -q_wire*Dx^2;
+            Adum(iR,iC:iC+1) = [-3 1];
+            Adum(iR+1,iC)    = 1;
+            Adum(iR-1,iC)    = 1;
+            b(iN) = -q_wire*Dx^2/k_c;
             type(iN) = 20;
 %%%%%%%%%%%%%%%%%%%%%_right walls_%%%%%%%%%%%%%%%%%%%%%%%
 % 
         elseif iR > 1 && iR < 40 && iC > 65 %do range for all right walls of insulation
             cas = 21;
-            Adum(iR,iC-1:iC) = [k_i -3];
-            Adum(iR+1,iC)    = k_i;
-            Adum(iR-1,iC)    = k_i;
+            Adum(iR,iC-1:iC) = [1 -3];
+            Adum(iR+1,iC)    = 1;
+            Adum(iR-1,iC)    = 1;
             type(iN) = 21;
 
         elseif iR > 39 && iR < 80 && iC > 65%do range for all right walls of tile
             cas = 22;
-            Adum(iR,iC-1:iC) = [k_t -3];
-            Adum(iR+1,iC)    = k_t;
-            Adum(iR-1,iC)    = k_t;
+            Adum(iR,iC-1:iC) = [1 -3];
+            Adum(iR+1,iC)    = 1;
+            Adum(iR-1,iC)    = 1;
             type(iN) = 22;
             
         elseif iR < 20 && iR > 9 && iC == 5 %do range for all right walls of copper
             cas = 23;
-            Adum(iR,iC-1:iC) = [k_c -3];
-            Adum(iR+1,iC)    = k_c;
-            Adum(iR-1,iC)    = k_c;
-            b(iN) = -q_wire*Dx^2;
+            Adum(iR,iC-1:iC) = [1 -3];
+            Adum(iR+1,iC)    = 1;
+            Adum(iR-1,iC)    = 1;
+            b(iN) = -q_wire*Dx^2/k_c;
             type(iN) = 23;
         
             
@@ -313,7 +316,7 @@ Tall(idx)= T; % reshape T vector to correspond to physical positions.
 % set(gca,'YDir','normal')
 
 
-figure(3);imagesc(Tall, [-5 30]) % display result ,[5*floor(min(T(:))/5) 5*ceil(max(T(:))/5)]
+figure(3);imagesc(Tall, [-5 600]) % display result ,[5*floor(min(T(:))/5) 5*ceil(max(T(:))/5)]
 set(gca,'YDir','normal')
 c = colorbar;
 c.Label.String = 'Temperature (?C)';
